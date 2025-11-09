156d26416fd2603c4cdce61ca40c2a0b81fadce4
Juanmaperea <juan.coronado@correounivalle.edu.co>
2025-11-09 13:20:48 -0500

    Compatibility Update
    Compatibility Update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.example.equipocinco"
+        applicationId = "com.univalle.equipocinco"
         minSdk = 26
         targetSdk = 34
         versionCode = 1
